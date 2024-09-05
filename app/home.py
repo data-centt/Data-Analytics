c2658da6479b4771f1af0ba384febe028125fa6f
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2024-09-05 15:11:10 +0100

    Update home.py

--- app/home.py
+++ app/home.py
@@ -29,33 +29,34 @@ if current_page == "Home":
         <style>
         body {
             background-color: inherit;
-        }
-        .container {
-            max-width: 850px;
-            margin: 0 auto;
-            padding: 20px;
-            border-radius: 10px;
-            background-color: rgba(255, 255, 255, 0.85);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
             text-align: center;
-            color: #000000; 
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
         }
+
         .home-title {
             font-size: 40px;
-            color: #000000; 
+            color: #2E86C1; 
             font-weight: bold;
             margin-bottom: 20px;
+            text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+            text-align: center;
         }
+
         .home-description {
             font-size: 18px;
             color: #333333; 
             margin-top: 10px;
             margin-bottom: 20px;
+            text-align: center;
         }
+
         .link-button {
             display: inline-block;
             font-size: 16px;
-            color: #FFFFFF; 
+            color: #29649e; 
             font-weight: bold;
             text-decoration: none;
             padding: 10px 20px;
@@ -63,45 +64,43 @@ if current_page == "Home":
             margin: 10px 5px;
             background-color: #007BFF; 
             transition: background-color 0.3s ease;
+            text-align: center;
         }
+
         .link-button:hover {
             background-color: #0056b3; 
         }
+
         .logo-container {
-            text-align: center;
-            border-radius: 10px;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            display: flex;
+            justify-content: center;
+            align-items: center;
             margin-bottom: 20px;
-            padding: 20px;
-            background-color: rgba(255, 255, 255, 0.9); 
         }
+
         .logo-container img {
             width: 200px;
             max-width: 100%;
             height: auto;
         }
-        .sub-container {
-            background-color: rgba(255, 255, 255, 0.9); 
-            padding: 15px;
-            border-radius: 10px;
-            margin-bottom: 20px;
-            text-align: center;
-            color: #000000; 
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
-        }
+
         .sub-title {
             font-size: 24px;
-            color: #000000; 
+            color: #2E86C1; 
             font-weight: bold;
             margin-bottom: 10px;
+            text-align: center;
         }
+
         .sub-description {
             font-size: 16px;
-            color: #333333; 
+             
             margin-bottom: 15px;
+            text-align: center;
         }
+
         .sub-button {
-            background-color: #28a745; 
+            background-color: #28a745; /* Bright green for action buttons */
             display: block;
             margin: auto;
             color: #FFFFFF; /* White text */
@@ -110,10 +109,13 @@ if current_page == "Home":
             border-radius: 5px;
             cursor: pointer;
             transition: background-color 0.3s ease;
+            text-align: center;
         }
+
         .sub-button:hover {
-            background-color: #218838; 
+            background-color: #218838; /* Slightly darker green on hover */
         }
+
         </style>
         """,
         unsafe_allow_html=True
@@ -123,37 +125,36 @@ if current_page == "Home":
 
     st.markdown(
         """
-        <div class="container">
             <div class="home-title">Data Analytics!</div>
             <p class="home-description">
                 
-This project is made to deliver powerful data analysis and visualization tools, offering a comprehensive platform for in-depth data exploration 📊. With this project, you can delve into a diverse range of datasets, perform detailed Exploratory Data Analysis (EDA) 🔍, and interact with dynamic dashboards designed to uncover valuable insights 📈. Whether you're a data enthusiast, a seasoned professional, or simply curious about data trends, this project is tailored to make data exploration both accessible and engaging for everyone 🌟.
+This project was made to deliver powerful data analysis and visualization tools, offering a comprehensive platform for in-depth data exploration 📊. With this project, you can delve into a diverse range of datasets, perform detailed Exploratory Data Analysis (EDA) 🔍, and interact with dynamic dashboards designed to uncover valuable insights 📈. Whether you're a data enthusiast, a seasoned professional, or simply curious about data trends, this project is tailored to make data exploration both accessible and engaging for everyone 🌟.
 
 For any issues you encounter while performing analysis or navigating through the webpage, please refer to the GitHub repository linked below to create an issue 🛠️. Alternatively, feel free to reach out to me directly via LinkedIn for personalized assistance 💬. Your feedback is crucial for improving the project and ensuring a smooth user experience 🚀.
+\n For maximum viewing and navigating experience, please turn on light-mode. It can be found in settings > theme
             </p>
             <div>
                 <a class="link-button github-link" href="https://github.com/Daniel15568/Data-Analytics-Dashboard" target="_blank">
-                    👉 Check out the GitHub Repository
+                    👉 GitHub Repository
                 </a>
                 <a class="link-button linkedin-link" href="https://www.linkedin.com/in/daniel15568" target="_blank">
-                    👨‍💻 Connect with me on LinkedIn
+                    👨‍💻 Connect on LinkedIn
                 </a>
             </div>
-        </div>
         """,
         unsafe_allow_html=True
     )
     with st.container():
         st.markdown(
             """
-            <div class="sub-container">
+            
                 <div class="sub-title">Data</div>
                 <p class="sub-description">
                     Upload and explore your datasets with ease! 📤 Whether you choose to upload your own data or utilize one of the provided sample datasets, you’ll have the tools to perform comprehensive analyses 🔍. Dive into detailed statistical summaries and gain a thorough understanding of your data with our intuitive interface 📊.
 
 Quickly access essential statistical insights and uncover full information about your datasets to drive informed decisions and discoveries. 📈✨
                 </p>
-            </div>
+            
             """,
             unsafe_allow_html=True
         )
@@ -163,14 +164,14 @@ Quickly access essential statistical insights and uncover full information about
         with st.container():
             st.markdown(
                 """
-                <div class="sub-container">
+                
                     <div class="sub-title">Dashboard</div>
                     <p class="sub-description">
                         View interactive dashboards that offer valuable insights into key metrics and trends within your data 🌟📊. Customize your dashboard to match your specific analytical needs, whether for business or personal analysis 🔧📈.
 
     Experience the power of interactive dashboards and data insights without the need to subscribe to or download any additional apps. Our dashboards, powered by the robust Plotly Express library, provide dynamic and visually engaging data visualizations 🚀📉. Enjoy open-source business analysis tools at no cost and gain comprehensive insights for free!
                     </p>
-                </div>
+               
                 """,
                 unsafe_allow_html=True
             )
@@ -180,19 +181,18 @@ Quickly access essential statistical insights and uncover full information about
     with st.container():
         st.markdown(
             """
-            <div class="sub-container">
-                <div class="sub-title">Exploratory Data Analysis ( EDA )</div>
+            
+                <div class="sub-title">\n Exploratory Data Analysis ( EDA )</div>
                 <p class="sub-description">
-                   Dive into Exploratory Data Analysis (EDA) with our collection of pre-made charts, created using the powerful Matplotlib and Seaborn visualization libraries 🎨📊. Whether you're preparing to apply machine learning techniques or simply looking to uncover statistical insights, these ready-to-use visualizations are designed to provide a deeper statistical understanding of your data 🔍📈. Easily identify patterns, trends, and key insights with these intuitive and informative graphics, and enhance your analytical capabilities effortlessly.
+                   \n \n \n Dive into Exploratory Data Analysis (EDA) with our collection of pre-made charts, created using the powerful Matplotlib and Seaborn visualization libraries 🎨📊. Whether you're preparing to apply machine learning techniques or simply looking to uncover statistical insights, these ready-to-use visualizations are designed to provide a deeper statistical understanding of your data 🔍📈. Easily identify patterns, trends, and key insights with these intuitive and informative graphics, and enhance your analytical capabilities effortlessly.
                    \n The logo obtained from https://miro.medium.com/v2/resize:fit:1400/format:webp/1*hf5w4xLfIfnr50ZYEUNOVw.jpeg
                 </p>
-            </div>
+            
             """,
             unsafe_allow_html=True
         )
         if st.button("Go to EDA Page"):
             navigate_to("EDA")
-
 
 
 elif current_page == "Data":
