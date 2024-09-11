d17411984014df65169b9f02e3ba321a8fbf7e50
Daniel Emakporuena <97764732+Daniel15568@users.noreply.github.com>
2024-09-11 19:48:49 +0100

    Update data.py

--- app/data.py
+++ app/data.py
@@ -35,24 +35,24 @@ def apply_css():
             padding-bottom: 5px;
         }
         .logo-container {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                margin-bottom: 20px;
-            }
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 20px;
+        }
 
-            .logo-container img {
-                width: 200px; 
-                max-width: 100%;
-                height: auto;
-            }
+        .logo-container img {
+            width: 200px; 
+            max-width: 100%;
+            height: auto;
+        }
         .home-description {
-                font-size: 18px;
-                color: var(--text-color-light); 
-                margin-top: 10px;
-                margin-bottom: 20px;
-                text-align: center;
-            }
+            font-size: 18px;
+            color: var(--text-color-light); 
+            margin-top: 10px;
+            margin-bottom: 20px;
+            text-align: center;
+        }
         .info-message {
             background-color: #f9ebea;
             border-left: 6px solid #D5DBDB;
@@ -81,10 +81,8 @@ def apply_css():
         unsafe_allow_html=True
     )
 
-
 def navigate_to(page_name):
     st.session_state["current_page"] = page_name
-
 
 def main():
     apply_css()
@@ -95,27 +93,32 @@ def main():
     st.markdown('<p class="main-title">Data Overview</p>', unsafe_allow_html=True)
 
     st.markdown("""
-            <p class="home-description">
-            This is where you upload your data, you have two options of data to test the app, choose a data and click ok for it to work,
-            After upload, simply press the 'X' to remove the upload window and continue. \n You can see the brief statistical details of your data, after getting the details, you can click the EDA button for Exploratory data analysis or the dashboard to enjoy the full experience of plotly express library.
-            \n Done with current data? simply double-click the reset button to upload/use another data.
-            </p>
-        """, unsafe_allow_html=True)
+        <p class="home-description">
+        This is where you upload your data, you have two options of data to test the app, choose a data and click ok for it to work,
+        After upload, simply press the 'X' to remove the upload window and continue. \n You can see the brief statistical details of your data, after getting the details, you can click the EDA button for Exploratory data analysis or the dashboard to enjoy the full experience of plotly express library.
+        \n Done with current data? simply double-click the reset button to upload/use another data.
+        </p>
+    """, unsafe_allow_html=True)
 
-    option = st.radio("Select Data:", ("Upload data", "Use sample data"))
+    # Initialize the session state for the radio option if it does not exist
+    if 'radio_option' not in st.session_state:
+        st.session_state['radio_option'] = "Upload data"
+
+    # Use the session state value for the radio button
+    option = st.radio("Select Data:", ("Upload data", "Use sample data"),
+                      index=("Upload data", "Use sample data").index(st.session_state['radio_option']))
+
+    # Update the session state when the radio button changes
+    if option != st.session_state['radio_option']:
+        st.session_state['radio_option'] = option
+
+    # Sample data paths
     path1 = "https://raw.githubusercontent.com/data-centt/Data-Analytics/main/sample%20data/fire.json"
     path2 = "https://raw.githubusercontent.com/data-centt/Data-Analytics/main/sample%20data/test.xlsx"
 
+    # Initialize session state for data management
     if 'df' not in st.session_state:
         st.session_state['df'] = None
-
-    if "data_source" not in st.session_state:
-        st.session_state["data_source"] = "Upload your own data"
-
-    if st.session_state["data_source"] != option:
-        st.session_state["data_source"] = option
-        st.session_state["df"] = None
-        st.session_state['file_uploaded'] = False
 
     if option == "Upload data":
         if 'file_uploaded' not in st.session_state:
@@ -132,27 +135,19 @@ def main():
             if st.button("Reset"):
                 st.session_state['file_uploaded'] = False
                 st.session_state['df'] = None
-                st.session_state["data_source"] = "Upload your own data"
 
     elif option == "Use sample data":
-        if "data_option" not in st.session_state:
-            st.session_state["data_option"] = "Pick data"
+        if 'data_option' not in st.session_state:
+            st.session_state['data_option'] = "Pick data"
 
         options = ["Pick data", "Fire data", "Employee data"]
-
         current_option = st.session_state.get("data_option", "Pick data")
 
-        if current_option not in options:
-            current_option = "Pick data"
+        # Keep the current option if already set, else reset to "Pick data"
+        data_option = st.selectbox("Choose data: ", options, index=options.index(current_option))
 
-        data_option = st.selectbox("Choose data: ", options,
-                                   index=options.index(current_option))
-
-        if st.session_state["data_option"] != data_option:
-            st.session_state["data_option"] = data_option
-            st.session_state["df"] = None
-
-        if st.session_state['df'] is None:
+        # Load data if not already loaded for the selected sample data option
+        if data_option != "Pick data" and st.session_state['df'] is None:
             if data_option == "Fire data":
                 response = requests.get(path1)
                 if response.status_code == 200:
@@ -161,43 +156,37 @@ def main():
                 response = requests.get(path2)
                 if response.status_code == 200:
                     st.session_state['df'] = pd.read_excel(io.BytesIO(response.content))
-            elif data_option == "Pick data":
-                st.write("Select a sample data from the drop-down menu")
-            else:
-                st.error("Seems like there are no sample data, please upload yours or reach out to me on LinkedIn.")
 
+        if data_option == "Pick data":
+            st.write("Select a sample data from the drop-down menu")
+
+    # If data exists in session state, display it
     if st.session_state['df'] is not None:
         df = st.session_state['df']
+        st.success("File uploaded successfully! 🎉 ✅")
 
-        if df is not None:
-            st.success("File uploaded successfully! 🎉 ✅")
+        tab1, tab2, tab3, tab4 = st.tabs(["Data Preview", "Data Information", "Column and Row Count", "Descriptive Analysis"])
 
-            tab1, tab2, tab3, tab4 = st.tabs(["Data Preview", "Data Information", "Column and Row Count",
-                                              "Descriptive Analysis"])
+        with tab1:
+            st.markdown('<p class="sub-header">Data Preview</p>', unsafe_allow_html=True)
+            st.write(df.head())
 
-            with tab1:
-                st.markdown('<p class="sub-header">Data Preview</p>', unsafe_allow_html=True)
-                st.write(df.head())
+        with tab2:
+            st.markdown('<p class="sub-header">Data Information</p>', unsafe_allow_html=True)
+            buffer = io.StringIO()
+            df.info(buf=buffer)
+            s = buffer.getvalue()
+            st.markdown(f"```{s}```")
 
-            with tab2:
-                st.markdown('<p class="sub-header">Data Information</p>', unsafe_allow_html=True)
-                buffer = io.StringIO()
-                df.info(buf=buffer)
-                s = buffer.getvalue()
-                st.markdown(f"```{s}```")
+        with tab3:
+            st.markdown('<p class="sub-header">Column and Row Count</p>', unsafe_allow_html=True)
+            st.write(f"Rows: {df.shape[0]}")
+            st.write(f"Columns: {df.shape[1]}")
 
-            with tab3:
-                st.markdown('<p class="sub-header">Column and Row Count</p>', unsafe_allow_html=True)
-                st.write(f"Rows: {df.shape[0]}")
-                st.write(f"Columns: {df.shape[1]}")
-
-            with tab4:
-                st.markdown('<p class="sub-header">Descriptive Analysis</p>', unsafe_allow_html=True)
-                st.markdown(
-                    '<div class="info-message">This gives the statistical value of the numerical columns in the '
-                    'dataframe, please note that other columns are omitted.</div>',
-                    unsafe_allow_html=True)
-                st.write(df.describe())
+        with tab4:
+            st.markdown('<p class="sub-header">Descriptive Analysis</p>', unsafe_allow_html=True)
+            st.markdown('<div class="info-message">This gives the statistical value of the numerical columns in the dataframe, please note that other columns are omitted.</div>', unsafe_allow_html=True)
+            st.write(df.describe())
 
     else:
         st.info("Please upload a file to start")
